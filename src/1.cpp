ad236533ad1d7ee0eca5cc6552289a104730ee79
swonke-joyce <swonke-joyce@gmx.com>
2025-03-03 14:16:30 +0000

    Add 1.cpp

---
+++ src/1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num = 10;
+    if (num > 5) {
+        cout << "The number is greater than 5" << endl;
+    } else {
+        cout << "The number is less than or equal to 5" << endl;
+    }
+}
