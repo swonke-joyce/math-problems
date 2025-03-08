08897873e387c676dbb1c172e04ba445d3549197
swonke-joyce <swonke-joyce@gmx.com>
2025-03-08 08:47:04 +0000

    Add 6.cpp

---
+++ src/6.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10;
+    int b = 20;
+    int result = a + b;
+    cout << "The sum of " << a << " and " << b << " is: " << result << endl;
+    return 0;
+}
